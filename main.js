683b0ad4e64490441cfcd7e142bd2dd9b136051e
Nimrajarral <nimrajarral@gmail.com>
2024-03-22 00:57:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var i = 10;
+var result = ++i + ++i + 6 + --i - 2;
+console.log(result);
+// concatination
+var name1 = "Nimra";
+var name2 = "Asif";
+console.log("".concat(name1, "  ").concat(name2));
---
+++ main.ts
@@ -0,0 +1,8 @@
+let i=10;
+let result= ++i + ++i + 6 + --i -2 ;
+console.log(result);
+
+    // concatination
+let name1 :string="Nimra";
+let name2:string="Asif";
+console.log(`${name1}  ${name2}`);
